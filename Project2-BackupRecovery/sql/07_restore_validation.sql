dfffa57b9b84d369c49d75b2905c987ce1784d98
Doğukan Filiz <79749288+dogukan-filiz@users.noreply.github.com>
2026-04-14 14:21:56 +0000

    Complete Project 2 backup and restore validation workflow

--- Project2-BackupRecovery/sql/07_restore_validation.sql
+++ Project2-BackupRecovery/sql/07_restore_validation.sql
@@ -2,7 +2,7 @@
 FROM DISK = 'C:\SQLBackups\Project2\full\AdventureWorks2022_Full_AfterRecovery.bak';
 GO*/
 
-USE master;
+/*USE master;
 GO
 
 IF DB_ID('AdventureWorks2022_RestoreTest') IS NOT NULL
@@ -28,4 +28,32 @@ GO
 SELECT name
 FROM sys.tables
 WHERE name = 'DisasterRecoveryTest';
+GO*/
+
+USE master;
+GO
+
+IF DB_ID('AdventureWorks2022_RestoreTest') IS NOT NULL
+BEGIN
+    ALTER DATABASE AdventureWorks2022_RestoreTest SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE AdventureWorks2022_RestoreTest;
+END
+GO
+
+RESTORE DATABASE AdventureWorks2022_RestoreTest
+FROM DISK = 'C:\SQLBackups\Project2\full\AdventureWorks2022_Full_PhoneTypeScenario.bak'
+WITH 
+    MOVE 'AdventureWorks2022' TO 'D:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\DATA\AdventureWorks2022_RestoreTest.mdf',
+    MOVE 'AdventureWorks2022_log' TO 'D:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\DATA\AdventureWorks2022_RestoreTest_log.ldf',
+    REPLACE,
+    RECOVERY,
+    STATS = 10;
+GO
+
+USE AdventureWorks2022_RestoreTest;
+GO
+
+SELECT PhoneNumberTypeID, Name, ModifiedDate
+FROM Person.PhoneNumberType
+ORDER BY PhoneNumberTypeID;
 GO
